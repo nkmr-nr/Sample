dc5ebcea1c3fb1a0eae1901203dfc94b9e960d31
DESKTOP-DEFOH0H\narun <narunaru318@outlook.jp>
2023-06-21 22:50:22 +0900

    Main.cppにforループの書き方を練習しました。

--- Sample/Main.cpp
+++ Sample/Main.cpp
@@ -1,7 +1,35 @@
 
 #include<iostream>
+#include<vector>
+#include <list>
 
 int main(void)
 {
+	const int Max = 10;
+	int ar[Max] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (int& i : ar)
+	{
+		if (i == 10)
+		{
+			printf("a\n");
+		}
+	}
+
+	std::list<int>list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(4);
+	list.push_back(5);
+	list.push_back(6);
+	list.push_back(7);
+	list.push_back(8);
+	list.push_back(9);
+	list.push_back(10);
+
+	for (auto &i : list)
+	{
+		printf("%d\n", i);
+	}
 
 }
